154d3e5b1490dc6e1fd87e9dfb15a2fcaeed04f5
willy <alexwguerrad@gmail.com>
2024-12-21 13:03:07 -0500

    Culminada la feature, inducir en unaprueba unitaria

--- CP2/Circunferencia/Program.cs
+++ CP2/Circunferencia/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Introduzca el radio de la circunferencia");
+double rad = double.Parse(Console.ReadLine()!);
+double Asc = (Math.PI * Math.Pow(rad, 2) * 60)/360;
+double Ate = (Math.Pow(rad, 2) * Math.Cbrt(3))/8;
+double Ac = 4*(Asc - Ate);
+Console.WriteLine($"El area de dicho circulo es {Ac}");

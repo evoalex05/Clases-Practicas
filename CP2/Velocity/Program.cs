8752255ca81b0db140889cf87416f86cf6276481
willy <alexwguerrad@gmail.com>
2024-12-21 22:28:42 -0500

    Ha culminado el desarrollo de Velocity

---
+++ CP2/Velocity/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Introduzca una oracion:");
+int time = Environment.TickCount;
+string text = Console.ReadLine()!;
+int time2 = Environment.TickCount;
+int result = time2 - time;
+Console.WriteLine($"Se ha demorado{result}");
+
+

ce0259b03ef86f45cf89c1502cbcfbaf8a23892b
willy <alexwguerrad@gmail.com>
2024-12-21 20:01:37 -0500

    Error corregido: Cbrt(3) en vez de Sqrt(3)

--- CP2/Circunferencia/Program.cs
+++ CP2/Circunferencia/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Introduzca el radio de la circunferencia");
 double rad = double.Parse(Console.ReadLine()!);
-double Asc = (Math.PI * Math.Pow(rad, 2) * 60)/360;
-double Ate = (Math.Pow(rad, 2) * Math.Cbrt(3))/8;
-double Ac = 4*(Asc - Ate);
+double Asc = (2 * Math.PI * rad * rad) / 3;
+double Ate = (Math.Sqrt(3) * rad * rad) / 2;
+double Ac = Asc - Ate;
 Console.WriteLine($"El area de dicho circulo es {Ac}");
+
